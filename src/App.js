fc7c11cc0e6268070e092d91b9b857ded23e0d3d
Nickoprisma <nicoviv864@gmail.com>
2022-02-20 14:19:27 -0300

    Initial App

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Discussion</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Questioning and understanding: this is not about strong positions, it is about expanding wisdom. 
         </p>
+        <img src={logo} className="App-logo" alt="logo ying yang" />
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://opensea.io/collection/questioning-and-understanding-expands-wisdom"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          See the entire collection
         </a>
       </header>
     </div>
